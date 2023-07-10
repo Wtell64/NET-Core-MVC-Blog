d4f8d9d4fb26777a9760eb3e7ba427ada0900fc8
Wtell64 <75840262+Wtell64@users.noreply.github.com>
2023-07-10 14:12:25 +0300

    Yorum classinda eksik olan bazi commentlar eklendi.

--- NETCoreMVCBlog/Models/Yorum.cs
+++ NETCoreMVCBlog/Models/Yorum.cs
@@ -4,7 +4,7 @@
 	{
 		public string KullaniciAdi { get; set; }
 		public DateTime Zaman { get; set; } = DateTime.Now;
-
+		// Yoruma gelen Begeni sayisi
 		public int BegenenSayisi { get; set; }
 		public int BegenmeyenSayisi { get; set; }
 	}
